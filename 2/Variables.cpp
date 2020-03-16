ff165c2340018f365f2a1317192f06fb9ca1e7d9
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2020-03-16 11:31:38 +0700

    Variables

---
+++ 2/Variables.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//	 Integer
+	int myInt = 10;
+	cout << "My Integer : " << myInt << "\n";
+	
+	//	 Double
+	double myDouble = 10.0;
+	cout << "My Double : " << myDouble << "\n";
+	
+	//	 Char
+	char myChar = 'A';
+	cout << "My Char : " << myChar << "\n";
+	
+	//	 String
+	string myString = "Aku Ganteng";
+	cout << "My String : " << myString << "\n";
+	
+	//	 Boolean
+	bool myBoolean = true;
+	cout << "My Boolean : " << myBoolean << "\n";
+}
