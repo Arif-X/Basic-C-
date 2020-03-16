6280cbcb82d5fa971c361351a6384d7bfa071738
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2020-03-16 12:07:21 +0700

    Input

---
+++ 4/Input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	float pi = 3.14;
+	int x;
+	cout << "Masukkan Jari-JariUntuk Mendapatkan Luas Lingkaran! \n";
+	cin >> x;
+	cout <<  "Luas Lingkaran : " << pow(x,2)*pi;
+}
