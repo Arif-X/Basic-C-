c83ce24158b945014452c223e15347e7fee95948
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2020-03-17 20:59:51 +0700

    Math

---
+++ 7/Math.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	//	Maximum
+	cout << "Maximum 3 to 10 : " << max(3, 10) << "\n";	
+	
+	//	Minimum
+	cout << "Minimum 3 to 10 : " << min(3, 10) << "\n";
+	
+	
+	//	Using <cmath> header	
+	//	SQRT
+	cout << "Sqrt 49 : " << sqrt(49) << "\n";
+	
+	//	Logaritma
+	cout << "Logaritma 2 : " << log(2) << "\n";
+	
+	//	Round atau Pembulatan
+	cout << "Round 4.5 : " << round(4.5) << "\n";
+	
+	//	Absolut
+	cout << "Absolut 4 : " << abs(4) << "\n";
+	
+	//	Sinus
+	cout << "Sinus 60 : " << sin(60) << "\n";
+	
+	//	Cosinus
+	cout << "Cosinus 60 : " << cos(60) << "\n";
+	
+	//	Tangen
+	cout << "Tange 60 : " << tan(60) << "\n";
+	
+	//	Arc Cosinus
+	cout << "Arc Cosinus 45 : " << acos(45) << "\n";
+	
+	//	Arc Sinus
+	cout << "Arc Sinus 45 : " << asin(45) << "\n";
+	
+	//	Arc Tangen
+	cout << "Arc Tangen 60 : " << atan(60) << "\n";
+	
+	//	CBRT atau Akar 3
+	cout << "Akar tiga dari 27 : " << cbrt(27) << "\n";
+		
+	//	Nilai F Maksimum
+	cout << "Nilai Maksimum dari 3 dan 7 : " << max(3, 7) << "\n";
+	
+	//	Nilai F Minimum
+	cout << "Nilai Minimum dari 3 dan 7 : " << min(3, 7) << "\n";
+	
+	//	Nilai EXP
+	cout << "EXP 5 : " << exp(5) << "\n";
+	
+	//	Nilai Pangkat
+	cout << "5 Pangkat 3 : " << pow(5, 3) << "\n";
+}
