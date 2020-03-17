d5f41040bced2df024a2227d3b299e3b16ae1cc4
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2020-03-18 05:21:04 +0700

    Switch

---
+++ 9/Switch Case.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int hari = 5;
+	switch(hari){	
+		case 1:
+			cout << "Minggu";
+			break;
+		case 2:
+			cout << "Senin";
+			break;
+		case 3:
+			cout << "Selasa";
+			break;
+		case 4:
+			cout << "Rabu";
+			break;
+		case 5:
+			cout << "Kamis";
+			break;
+		case 6:
+			cout << "Jumat";
+			break;
+		case 7:
+			cout << "Sabtu";
+			break;
+	}
+}
